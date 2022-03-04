57909881c665a97b2f7802a48c4652221c1e04ab
ye da goat fr <100773794+tankmankirils1989@users.noreply.github.com>
2022-03-04 10:36:53 +0200

    updatekbh.cpp
    kbh

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 using namespace std;
-int process (int x, int y, int & max, double & avg) 
+int process (int x, int y, int & max, int diff, double & avg) 
 {
-
+if(x>y){
+  diff = x-y;
+  max = x;
+  avg= (x+y)/2;
+    }
+  else {
+    max = y;
+    diff = y-x;
+    avg= (x+y)/2;
+  }
 }
 int main() {
-	int a,b,liel;
-	double vid;
+	int a,b,max;
+	double avg;
 	cout << "Ievadiet skaitļus:\n";
 	cin >> a >> b;
-	int diff=process(a,b,liel,vid);
+	int diff= process (a,b,max,diff,avg);
 	cout << "Lielākais: "
-		<< liel << endl;
+		<< max << endl;
 	cout << "Vidējais: "
-		<< vid	<< endl;
+		<< avg	<< endl;
 	cout << "Starpība: "
 		<< diff << endl;
 	return 0;
